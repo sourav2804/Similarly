00c4a032082c7b4744adeb3defc2cf1b1f4095b0
Sourav <souravnaskar2804@gmail.com>
2022-03-12 23:11:50 -0600

    made homepage and added views and icons

--- similarly_app/App.js
+++ similarly_app/App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import * as React from 'react';
+import { StyleSheet, Text, View, Button, Pressable, Image } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.createTagArea}>
+        <Text>sim</Text>
+      </View>
+      <View style={styles.myTagArea}>
+        <Text>sim</Text>
+      </View>
+      <View style={styles.BottomNavigationBar}>
+        <View style={styles.navigationButtons}>
+          <Pressable style={styles.iconStyle}>
+            <Image source={require("./Icons/home.png")} />
+          </Pressable>
+        </View>
+        <View style={styles.navigationButtons}>
+          <Pressable style={styles.iconStyle}>
+            <Image source={require("./Icons/explore.png")} />
+          </Pressable>
+        </View>
+        <View style={styles.navigationButtons}>
+          <Pressable style={styles.iconStyle}>
+            <Image source={require("./Icons/profile.png")} />
+          </Pressable>
+        </View>
+      </View>
     </View>
+
+    // <StatusBar style="auto" />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  BottomNavigationBar: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "grey",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  myTagArea: {
+    flex: 6,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  createTagArea: {
+    flex: 3,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  navigationButtons: {
+    flex: 3,
+    alignItems: "center",
+  },
+  iconStyle: {
+    // justifyContent:"space-evenly",
+    resizeMode: "contain",
   },
 });
